1c5e0ab575bc2cef435f5f6f63756fd14e870ae7
BoguszDmowski <58112552+BoguszDmowski@users.noreply.github.com>
2020-06-30 19:59:13 +0200

    Header MediaQuery fix 1/2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
-import { Menu, Segment } from 'semantic-ui-react'
-// import NewsFiltersBar from '../Main/HomePage/NewsFiltersBar/NewsFiltersBar';
+import { Menu, Dropdown, Segment } from 'semantic-ui-react'
 import {Link} from 'react-router-dom';
 import Languages from './Languages';
 
@@ -16,31 +15,50 @@ export default class Header extends Component {
     window.addEventListener("resize", this.reportWindowSize);
   }
   render() {
-    const { activeItem } = this.state
+    const { activeItem, layoutType } = this.state
 
     return (
       <Segment inverted>
         <Menu inverted secondary>
-          <Menu.Item
-            as= {Link}
-            to='/'
-            name='home'
-            active={activeItem === 'home'}
-            onClick={this.handleItemClick}
-          />
-          <Menu.Item
-            as= {Link}
-            to='/hobby'
-            name='hobby'
-            active={activeItem === 'hobby'}
-            onClick={this.handleItemClick}
-          />
-          <Menu.Item
-            as= {Link}
-            to='/help'
-            name='help'
-            active={activeItem === 'help'}
-          />
+          {layoutType !== layoutType.LANDSCAPE ? (
+                <Dropdown item icon="align justify">
+                  <Dropdown.Menu>
+                    <Dropdown.Item as={Link} to="/" exact>
+                      Home
+                    </Dropdown.Item>
+                    <Dropdown.Item as={Link} to="/hobby">
+                      HobbyPage
+                    </Dropdown.Item>
+                    <Dropdown.Item as={Link} to="/help">
+                      Help
+                    </Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              ) : (
+            <>    
+              <Menu.Item
+                as= {Link}
+                to='/'
+                name='home'
+                active={activeItem === 'home'}
+                onClick={this.handleItemClick}
+              />
+              <Menu.Item
+                as= {Link}
+                to='/hobby'
+                name='hobby'
+                active={activeItem === 'hobby'}
+                onClick={this.handleItemClick}
+              />
+              <Menu.Item
+                as= {Link}
+                to='/help'
+                name='help'
+                active={activeItem === 'help'}
+              />
+            </>
+              )
+            }
           <Menu.Menu position='right'>
             <Menu.Item>
               <Languages onLanguageChange={this.props.onLanguageChange} />
