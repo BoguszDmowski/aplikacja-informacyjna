0716c84927757aba26af0c0f52a3d9f7ae6cd968
BoguszDmowski <58112552+BoguszDmowski@users.noreply.github.com>
2020-06-30 20:13:05 +0200

    Header MediaQuery 2/2: Query fixed, menu bug added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Menu, Dropdown, Segment } from 'semantic-ui-react'
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import Languages from './Languages';
 
 export default class Header extends Component {
@@ -8,28 +8,39 @@ export default class Header extends Component {
   handleItemClick = (e, { name }) => this.setState({ activeItem: name })
 
   reportWindowSize = () => {
-    if (window.innerWidth < 768) return this.layoutType = 'MOBILE';
-    else return this.layoutType = 'LANDSCAPE';
-  }
+    if (window.innerWidth < 768) {
+      this.setState({ layoutType: "MOBILE" });
+    } else {
+      this.setState({ layoutType: "LANDSCAPE" });
+    }
+  }; 
+  //   return this.layoutType = 'MOBILE';
+  //   else return this.layoutType = 'LANDSCAPE';
+  // }
   componentDidMount() {
     window.addEventListener("resize", this.reportWindowSize);
+    this.reportWindowSize();
+  }
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.reportWindowSize);
+    this.reportWindowSize();
   }
   render() {
     const { activeItem, layoutType } = this.state
-
+  
     return (
       <Segment inverted>
         <Menu inverted secondary>
-          {layoutType !== layoutType.LANDSCAPE ? (
+          {layoutType === 'MOBILE' ? (
                 <Dropdown item icon="align justify">
                   <Dropdown.Menu>
-                    <Dropdown.Item as={Link} to="/" exact>
+                    <Dropdown.Item as={NavLink} to="/" exact>
                       Home
                     </Dropdown.Item>
-                    <Dropdown.Item as={Link} to="/hobby">
+                    <Dropdown.Item as={NavLink} to="/hobby">
                       HobbyPage
                     </Dropdown.Item>
-                    <Dropdown.Item as={Link} to="/help">
+                    <Dropdown.Item as={NavLink} to="/help">
                       Help
                     </Dropdown.Item>
                   </Dropdown.Menu>
@@ -37,21 +48,21 @@ export default class Header extends Component {
               ) : (
             <>    
               <Menu.Item
-                as= {Link}
+                as= {NavLink}
                 to='/'
                 name='home'
                 active={activeItem === 'home'}
                 onClick={this.handleItemClick}
               />
               <Menu.Item
-                as= {Link}
+                as= {NavLink}
                 to='/hobby'
                 name='hobby'
                 active={activeItem === 'hobby'}
                 onClick={this.handleItemClick}
               />
               <Menu.Item
-                as= {Link}
+                as= {NavLink}
                 to='/help'
                 name='help'
                 active={activeItem === 'help'}
